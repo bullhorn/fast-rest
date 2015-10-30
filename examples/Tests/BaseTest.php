2328ca1866aa41a0da91b1d3ca53e86d49507781
Nicholas Lewis <nlewis@bullhorn.com>
2015-10-30 09:50:30 -0400

    Added PropertyTester for unit tests

---
+++ examples/Tests/BaseTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Tests;
+use Bullhorn\FastRest\UnitTestHelper\Base as UnitTestHelperBase;
+use Base;
+use Bullhorn\FastRest\UnitTestHelper\ClassPropertyTest;
+
+class BaseTest extends UnitTestHelperBase {
+
+	/**
+	 * Tests the getters, setters, and adders for all properties
+	 * @return VOID
+	 */
+	public function testProperties() {
+		$objectFactory = function () {
+			return new Base();
+		};
+		$tester = new ClassPropertyTest($objectFactory, $this);
+		$tester->testAllPropertiesGettersAndSetters();
+	}
+
+	/**
+	 * Tests the getters, setters, and adders for all properties
+	 * @return VOID
+	 */
+	public function testProperties_abstractClass() {
+		$objectFactory = function () {
+			$base = $this->getMockBuilder(Base::class)
+				->setMethods(['listOfAbstractMethods'])
+				->getMock();
+			return $base;
+		};
+		$tester = new ClassPropertyTest($objectFactory, $this, Base::class);
+		$tester->testAllPropertiesGettersAndSetters();
+	}
+
+}
