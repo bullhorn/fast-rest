650e90decc21978f292e4d8a13da72192ce465a7
Nicholas Lewis <nlewis@bullhorn.com>
2017-07-13 14:17:44 -0400

    Add support for more rules for pluralizing

--- Library/Bullhorn/FastRest/Generator/PluralHelper.php
+++ Library/Bullhorn/FastRest/Generator/PluralHelper.php
@@ -2,6 +2,13 @@
 namespace Bullhorn\FastRest\Generator;
 
 class PluralHelper {
+    const RULES = [
+        '@(^d|D)ay$@' => '\\1ays',
+        '@ch$@' => 'ches',
+        '@y$@' => 'ies',
+        '@$@' => 's'
+    ];
+
     /**
      * pluralizes a string
      *
@@ -10,13 +17,10 @@ class PluralHelper {
      * @return string
      */
     public function pluralize($string) {
-        if(preg_match('@ch$@', $string)) {
-            $string .= 'es';
-        } elseif(preg_match('@y$@', $string)) {
-            $string = substr($string, 0, -1) . 'ies';
-        } else {
-            $string .= 's';
+        foreach(self::RULES as $rule => $replace) {
+            if(preg_match($rule, $string)) {
+                return preg_replace($rule, $replace, $string);
+            }
         }
-        return $string;
     }
 }
