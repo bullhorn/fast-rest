3c00464875763d9883ce9ce9c3d5a440a8c44edf
Nick Lewis <nlewis@bullhorn.com>
2019-02-26 16:34:27 -0500

    Handle No Controller found

--- Library/Bullhorn/FastRest/Api/Services/Config/Services.php
+++ Library/Bullhorn/FastRest/Api/Services/Config/Services.php
@@ -5,6 +5,7 @@ use Bullhorn\FastRest\Api\Services\Database\Connections;
 use Bullhorn\FastRest\DependencyInjection;
 use Phalcon\Config;
 use Phalcon\Di\InjectionAwareInterface;
+use \Phalcon\Mvc\Dispatcher as PhDispatcher;
 
 class Services implements InjectionAwareInterface {
     use DependencyInjection;
@@ -41,5 +42,31 @@ class Services implements InjectionAwareInterface {
      */
     private function addApiConfig() {
         $this->getDi()->setShared(ApiConfig::DI_NAME, new ApiConfig());
+
+        $di = $this->getDi();
+        $di->set(
+            'dispatcher',
+            function() use ($di) {
+
+                $evManager = $di->getShared('eventsManager');
+
+                $evManager->attach(
+                    "dispatch:beforeException",
+                    function($event, $dependencyInjectorspatcher, $exception)
+                    {
+                        switch ($exception->getCode()) {
+                            case PhDispatcher::EXCEPTION_HANDLER_NOT_FOUND:
+                            case PhDispatcher::EXCEPTION_ACTION_NOT_FOUND:
+                                return false;
+                        }
+                    }
+                );
+                $dependencyInjectorsPatcher = new PhDispatcher();
+                $dependencyInjectorsPatcher->setEventsManager($evManager);
+                return $dependencyInjectorsPatcher;
+            },
+            true
+        );
+
     }
 }
