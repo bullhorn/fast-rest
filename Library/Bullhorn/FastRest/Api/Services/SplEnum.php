d88edc311780d6027dcca5ed3aece74d8f330653
Nick Lewis <nlewis@bullhorn.com>
2019-05-08 15:50:22 -0400

    Move SplEnum to services

--- Library/Bullhorn/FastRest/Api/Services/SplEnum.php
+++ Library/Bullhorn/FastRest/Api/Services/SplEnum.php
@@ -1,34 +1,6 @@
 <?php
 namespace Bullhorn\FastRest\Api\Services;
+use Bullhorn\FastRestServices\SplEnum as ParentSplEnum;
 //Fix for default SplEnum not being available in windows
-abstract class SplEnum implements \JsonSerializable {
-    /** @var  string */
-    private $value;
-
-    final public function __construct($value) {
-        $c = new \ReflectionClass($this);
-        if(!in_array($value, $c->getConstants())) {
-            throw new \InvalidArgumentException('Invalid Value: '.$value);
-        }
-        $this->value = $value;
-    }
-
-    /**
-     * getValues
-     * @return array
-     */
-    public static function getValues() {
-        $c = new \ReflectionClass(static::class);
-        return $c->getConstants();
-    }
-
-    final public function __toString() {
-        return $this->value;
-    }
-
-    public function jsonSerialize() {
-        return $this->__toString();
-    }
-
-
+abstract class SplEnum extends ParentSplEnum {
 }
