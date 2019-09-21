d1103498f34ea1354f0a54ea32266c37248b50a3
Nick Lewis <nlewis@bullhorn.com>
2019-09-20 22:52:19 -0400

    Fix for date

--- Library/Bullhorn/FastRest/Api/Services/Date/DateTime.php
+++ Library/Bullhorn/FastRest/Api/Services/Date/DateTime.php
@@ -1,5 +1,5 @@
 <?php
 namespace Bullhorn\FastRest\Api\Services\Date;
-class DateTime extends \Bullhorn\FastRestServices\Date\Date {
+class DateTime extends \Bullhorn\FastRestServices\Date\DateTime {
 
 }
